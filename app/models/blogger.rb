cdfb876b6749fe087b257b16b2cbd0c298760869
github username <github email address>
2020-02-06 21:25:21 -0500

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -13,4 +13,26 @@ def total_likes
     likes.sum
 end 
 
+def top_post
+    tp = self.posts.order(:likes).first
+end 
+
+def tf_dests
+    dest_frequencies = Hash.new 
+    self.destinations.each do |dn| 
+        if dest_frequencies[dn] 
+        dest_frequencies[dn] += 1
+        else 
+        dest_frequencies[dn]= 1    
+        end
+    end  
+    dest_frequencies
+end  
+
+def tf_names
+    dests = self.tf_dests.keys 
+    dests.map{|d| "#{d.name} : #{d.country}"}
+end 
+
+
 end
